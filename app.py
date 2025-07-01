dfc45da6d5fc9e866d2c05257dd526f7a8d7fd82
Olexandr Zey <alexandr.zey@gmail.com>
2025-07-01 23:31:08 +0300

    Switch from SQLite to PostgreSQL

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Концфігурація бази даних (SQLite)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
