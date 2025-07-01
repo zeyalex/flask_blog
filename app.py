26474c59f1117ba5e70e23f37100160466fe3620
Olexandr Zey <alexandr.zey@gmail.com>
2025-07-01 20:10:34 +0300

    Make Flask app compatible with Railway deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -12,4 +13,5 @@ def home():
     return render_template('home.html', posts=posts)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
